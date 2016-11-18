2f2b502a4ae6fc4ea13e70dbcc42c4f93d7a7273
shwan2016 <shwan.it2016@gmail.com>
2016-11-17 22:29:23 -0600

    Add project files.

---
+++ CarRental/Controllers/CarsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarRental.Models;
+
+namespace CarRental.Controllers
+{
+    public class CarsController : Controller
+    {        
+        public ActionResult Index()
+        {
+            var cars = GetCars();
+            return View(cars);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var car = GetCars().SingleOrDefault(c => c.Id == id);
+            return View(car);
+        }
+
+        private IEnumerable<Car> GetCars()
+        {
+            return new List<Car>()
+            {
+                new Car {Id = 1, Name = "Hundai"},
+                new Car {Id = 2, Name = "Toyota"}
+            };
+        }
+
+        }
+}
---
+++ CarRental/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebSockets;
+using CarRental.Models;
+
+namespace CarRental.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id = 1, Name = "Shwan Mustafa"},
+                new Customer {Id = 2, Name = "Hama Bahadin"}
+            };
+        }
+    }
+}
+
