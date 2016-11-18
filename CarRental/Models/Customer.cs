c0814c348a06624c3a3e8ebe07ba40f8760ba665
shwan2016 <shwan.it2016@gmail.com>
2016-11-18 13:08:57 -0600

    Add Details To Customers

--- CarRental/Models/Customer.cs
+++ CarRental/Models/Customer.cs
@@ -12,6 +12,7 @@ namespace CarRental.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        public DateTime? BirthDate { get; set; }
         public bool IsSubscribed { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
